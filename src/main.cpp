ec9f67ba0ebe28dd5f8b1c3c6076ef5c690c3a81
David Awad <davidawad64@gmail.com>
2017-06-21 15:58:28 -0400

    MAINT: pulling latest version of the project

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main() {
           double v = j[1]["speed"];
 
           /*
-          * TODO: Calculate steeering angle and throttle using MPC.
+          * TODO: Calculate steering angle and throttle using MPC.
           *
           * Both are in between [-1, 1].
           *
@@ -107,7 +107,7 @@ int main() {
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          //Display the MPC predicted trajectory
+          //Display the MPC predicted trajectory 
           vector<double> mpc_x_vals;
           vector<double> mpc_y_vals;
 
